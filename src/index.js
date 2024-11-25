6abacce8c59775f30a8dad30abb8a9391f9b397c
tog <96715682+utogtokh6@users.noreply.github.com>
2024-11-25 22:04:43 +0800

    HashRouter bolgov BrowserRouter-iig

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <HashRouter><App /></HashRouter>
 
   </React.StrictMode>
 );
